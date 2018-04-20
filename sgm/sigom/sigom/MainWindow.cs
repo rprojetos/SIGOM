83886073fc288b9b9a1dadd1433685d2e496b32a
rprojetos <rprojetos.ti@hotmail.com>
2018-04-19 23:34:12 -0300

    Estrutura básica do projeto SIGOM e inf. sobre GTK

---
+++ sgm/sigom/sigom/MainWindow.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Gtk;
+
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnBtnShowMsgClicked(object sender, EventArgs e)
+    {
+        // Cria dialogo
+        MessageDialog dlg = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.YesNo, "Hello World!");
+
+        dlg.Response += delegate (object o, ResponseArgs resp)
+        {
+            if (resp.ResponseId == ResponseType.Yes)
+            {
+                label1.Text = "Button Yes";
+
+            }
+            if (resp.ResponseId == ResponseType.No)
+            {
+                label1.Text = "Button No";
+
+            }
+        };
+
+        // chama dialogo
+        dlg.Run();
+        // Destroi diaglogo
+        dlg.Destroy();
+
+
+        MessageDialog md = new MessageDialog(this,
+        DialogFlags.DestroyWithParent, MessageType.Info,
+        ButtonsType.Close, "Download completed");
+        md.Run();
+        md.Destroy();
+
+        sigom.WorkshopWindow w = new sigom.WorkshopWindow();
+        w.Show();
+
+        this.Destroy();
+    }
+}
---
+++ sgm/sigom/sigom/WorkshopWindow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace sigom
+{
+    public partial class WorkshopWindow : Gtk.Window
+    {
+        public WorkshopWindow() :
+                base(Gtk.WindowType.Toplevel)
+        {
+            this.Build();
+        }
+    }
+}
