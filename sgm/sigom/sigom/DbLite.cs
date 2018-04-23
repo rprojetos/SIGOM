4e34c44ff7ddb75f3e6500fd05c4d9af24f66dca
rprojetos <rprojetos.ti@hotmail.com>
2018-04-23 06:31:44 -0300

    versao 0.5

---
+++ sgm/sigom/sigom/DbLite.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Gtk;
+using System.Collections;
+using System.Collections.Generic;
+//using Mono.Data.Sqlite;
+//using System.Data.SQLite;
+namespace sigom
+{
+    public class DbLite
+    {
+        public DbLite()
+        {
+        }
+
+        public void showMessage(String msg){
+            MessageDialog md = new MessageDialog(null ,
+            DialogFlags.DestroyWithParent, MessageType.Info,
+            ButtonsType.Close, msg);
+            md.Run();
+            md.Destroy();
+        }
+
+        public void dbCreate(String dbPathFile)           
+        {
+            try{
+                if (System.IO.File.Exists(dbPathFile))
+                    System.IO.File.Delete(dbPathFile);
+
+                Mono.Data.Sqlite.SqliteConnection.CreateFile(dbPathFile);
+                showMessage("O db "+ dbPathFile +", foi criado com sucesso!");
+            }
+            catch(Exception ex){
+                showMessage("Erro ao criar db: " + ex.Message);
+            }
+        }
+
+        public void dbSetCommand(String dbPathFile, String commands)
+        {
+            bool exists = System.IO.File.Exists(dbPathFile);
+            if (exists)
+            {
+                try
+                {
+                    showMessage(dbPathFile);
+                    showMessage(commands);
+                    Mono.Data.Sqlite.SqliteConnection connection =
+                        new Mono.Data.Sqlite.SqliteConnection("Data Source=" + dbPathFile);
+                    connection.Open();
+                    Mono.Data.Sqlite.SqliteCommand dbcmd = connection.CreateCommand();
+                    dbcmd.CommandText = commands;
+                    dbcmd.ExecuteNonQuery();
+                    connection.Close();
+                    showMessage("SqliteCommand Executado com sucesso!");
+                }
+                catch (Exception ex)
+                {
+                    showMessage("Erro ao executar SqliteCommand: " + ex.Message);
+                }
+            }
+            else
+            {
+                showMessage("O db " + dbPathFile + ", não foi encontrado.");
+            }
+        }
+
+        public String dbGetCommand(String dbPathFile, String tableName, String resultCol, String refCol, String refData)
+        {
+            int i = 0;
+            String resultModel = "";
+            String strQuery = "SELECT * FROM " + tableName;
+            ArrayList readArray = new ArrayList();
+            try
+            {
+                showMessage(dbPathFile);
+                showMessage(tableName);
+                Mono.Data.Sqlite.SqliteConnection connection =
+                    new Mono.Data.Sqlite.SqliteConnection("Data Source=" + dbPathFile);
+                connection.Open();
+                Mono.Data.Sqlite.SqliteCommand dbcmd = connection.CreateCommand();
+
+                dbcmd.CommandText = strQuery;
+
+                //SqliteDataReader rdr = cmd.ExecuteReader()
+                Mono.Data.Sqlite.SqliteDataReader rdr = dbcmd.ExecuteReader();
+
+
+                while (rdr.Read())
+                {
+                    i++;
+                    if(rdr[refCol].ToString() == refData){
+                        resultModel = rdr[resultCol].ToString();
+                    }
+                }
+
+                connection.Close();
+                showMessage("SqliteDataReader Executado com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                showMessage("Erro ao executar SqliteDataReader: " + ex.Message);
+            }
+
+            return resultModel;
+        }
+    }
+}
--- sgm/sigom/sigom/MainWindow.cs
+++ sgm/sigom/sigom/MainWindow.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Gtk;
-
+using System.Collections;
+using sigom;
 
 public partial class MainWindow : Gtk.Window
 {
@@ -17,38 +18,105 @@ public partial class MainWindow : Gtk.Window
 
     protected void OnBtnShowMsgClicked(object sender, EventArgs e)
     {
+        
         // Cria dialogo
-        MessageDialog dlg = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.YesNo, "Hello World!");
+        //MessageDialog dlg = new MessageDialog(null, DialogFlags.Modal, MessageType.Info, ButtonsType.YesNo, "Hello World!");
 
-        dlg.Response += delegate (object o, ResponseArgs resp)
-        {
-            if (resp.ResponseId == ResponseType.Yes)
-            {
-                label1.Text = "Button Yes";
+        //dlg.Response += delegate (object o, ResponseArgs resp)
+        //{
+        //    if (resp.ResponseId == ResponseType.Yes)
+        //    {
+        //        label1.Text = "Button Yes";
 
-            }
-            if (resp.ResponseId == ResponseType.No)
-            {
-                label1.Text = "Button No";
-
-            }
-        };
+        //    }
+        //    if (resp.ResponseId == ResponseType.No)
+        //    {
+        //        label1.Text = "Button No";
+        //
+        //    }
+        //};
 
         // chama dialogo
-        dlg.Run();
+        //dlg.Run();
         // Destroi diaglogo
-        dlg.Destroy();
+        //dlg.Destroy();
 
+
+        //MessageDialog md = new MessageDialog(this,
+        //DialogFlags.DestroyWithParent, MessageType.Info,
+        //ButtonsType.Close, "Download completed");
+        //md.Run();
+        //md.Destroy();
+
+        String path = System.IO.Directory.GetCurrentDirectory() + "/";
+        String dbName = "teste03.db";
+
+        String dbPathFile = path + dbName;
+
+        DbLite dbl = new DbLite();
+        //dbl.showMessage("Usando a classe DbLite.cs");
+
+        //dbl.dbCreate(dbPathFile);
+
+        //criando uma tabela
+        ///string createTable = @"CREATE TABLE Pessoa (ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,Nome VARCHAR(100) NOT NULL,Telefone VARCHAR (30))";
+        ///dbl.dbSetCommand(dbPathFile, createTable);
+
+        //Realizando um alter table --> Nome da coluna(DataNascimento) e o seu tipo(DATE)
+        ///String alterTable = "ALTER TABLE Pessoa ADD Column DataNascimento VARCHAR(10)";
+        ///dbl.dbSetCommand(dbPathFile, alterTable);
+
+        //Inserir dados na tabela:
+        ///String Nome = "Ricardo Poleto Ribeiro";
+        ///String Telefone = "37-99937-5038";
+        ///String data = "08/11/1977";
+        ///String insertCommand = "INSERT INTO Pessoa (Nome, Telefone, DataNascimento)"+ 
+            ///"VALUES('"+Nome+"','"+Telefone+"','"+data+"')";
+        ///dbl.dbSetCommand(dbPathFile, insertCommand);
+
+        //Update: 
+        ///String uNome = "Ricardo Poleto Ribeiro";
+        ///String uTelefone = "37-99937-5038";
+        ///String udata = "08/11/1977";
+        ///int id = 1;
+        ///String insertCommand = "UPDATE Pessoa SET "+ 
+            ///"Nome = '"+uNome+"', Telefone = '"+uTelefone+"', DataNascimento ='"+udata+"' WHERE ID = "+ id;
+        ///dbl.dbSetCommand(dbPathFile, insertCommand);
+
+        //DELETE
+        ///int id = 2;
+        ///string deleteCommand = "DELETE FROM Pessoa WHERE ID = " + id;
+        ///dbl.dbSetCommand(dbPathFile, deleteCommand);
+
+        //int id = 3;
+        //string getCommand = "SELECT * FROM Pessoa WHERE ID = " + id;
+        String tableName = "Pessoa";
+        String getData = dbl.dbGetCommand(dbPathFile, tableName, "Nome", "ID", "3");
+
+        //foreach (object values in getData)
+        //{
+            //Console.WriteLine(values);
+        //    MessageDialog md = new MessageDialog(this,
+        //    DialogFlags.DestroyWithParent, MessageType.Info,
+        //                                         ButtonsType.Close, values.ToString());
+        //    md.Run();
+        //    md.Destroy();
+        //} 
+
+        //String dbPathFile, String tableName, String resultCol, String refCol, String refData
 
         MessageDialog md = new MessageDialog(this,
-        DialogFlags.DestroyWithParent, MessageType.Info,
-        ButtonsType.Close, "Download completed");
-        md.Run();
-        md.Destroy();
+            DialogFlags.DestroyWithParent, MessageType.Info,
+                                             ButtonsType.Close, getData);
+            md.Run();
+            md.Destroy();
 
-        sigom.WorkshopWindow w = new sigom.WorkshopWindow();
-        w.Show();
+        //sigom.WorkshopWindow w = new sigom.WorkshopWindow();
+        //w.Show();
 
-        this.Destroy();
+        //this.Destroy();
     }
 }
+
+//ref.Continua ...
+//http://techblog.desenvolvedores.net/2011/05/02/csharp-com-sqlite/
