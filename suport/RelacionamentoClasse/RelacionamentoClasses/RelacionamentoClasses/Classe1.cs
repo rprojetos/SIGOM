38b656a66e397e185d0d6ae3b3c3afe90ec3539e
rprojetos <rprojetos.ti@hotmail.com>
2018-05-04 07:34:08 -0300

    .

---
+++ suport/RelacionamentoClasse/RelacionamentoClasses/RelacionamentoClasses/Classe1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+
+namespace RelacionamentoClasses
+{
+    public class Classe1
+    {
+        private String fc1 = "Frase da classe 1";
+
+        private Classe2[] c2 = new Classe2[15];//
+
+        private ArrayList alC2 = new ArrayList();
+
+        Hashtable hashTable = new Hashtable();
+
+        private Classe2 lc2;
+        private Classe2 htc2;
+
+        public Classe1()
+        {
+            c2[0].printC2();
+
+            alC2.Add(lc2);
+            alC2.Add(lc2);
+            alC2.Add(lc2);
+            alC2.Add(lc2);
+            alC2.Add(lc2);
+            alC2.Add(lc2);
+
+            
+        }
+
+        public void printC1(){
+            Console.WriteLine(fc1);
+
+            alC2[0].;
+        }
+    }
+}
---
+++ suport/RelacionamentoClasse/RelacionamentoClasses/RelacionamentoClasses/Classe2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RelacionamentoClasses
+{
+    public class Classe2
+    {
+        private String fc2 = "Frase da classe 2";
+
+        public Classe2()
+        {
+            
+        }
+
+        public void printC2(){
+            Console.WriteLine(fc2);
+        }
+    }
+}
